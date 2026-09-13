e2e2f32474b0df89a0defa6f26dac0ae87f30b82
MAHDI RAEISI <m9121724160@GMAIL.COM>
2026-09-13 06:12:13 +0300

    test(finance): verify ledger flows in both mobile browser engines

--- tests/e2e/finance-journal-webkit.spec.ts
+++ tests/e2e/finance-journal-webkit.spec.ts
@@ -1,4 +1,4 @@
 import { test, devices } from "@playwright/test";
 import { journalBrowserFlows } from "./helpers/journal-flow";
-test.use({ ...devices["Pixel 7"], browserName: "chromium" });
-test.describe("Chromium", journalBrowserFlows);
+test.use({ ...devices["iPhone 13"], browserName: "webkit" });
+test.describe("WebKit", journalBrowserFlows);
